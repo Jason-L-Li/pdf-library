edf2532a74c98de9e33eff3f20a8cdf3646b5b69
Jason <jason.lixing.li@gmail.com>
2014-10-11 19:02:32 -0500

    Initial commit - multiline text functional

---
+++ Document.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace PdfWriter
+{
+    public class Document
+    {
+        List<string> content; // TODO: Implement as List<PdfObject>
+        Xref xref;
+        string location;
+        StorageFile dataFile;
+        int objectCounter = 6;
+
+        public Document(string filename)
+        {
+            location = filename;
+            content = new List<string>();
+            xref = new Xref();
+
+            // Add header and catalog object
+            content.Add("%PDF-1.4");
+            content.Add("1 0 obj <</Type /Catalog /Pages 2 0 R>>");
+            content.Add("endobj");
+            
+            // Add Pages parent object
+            content.Add("2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>>"); // This is line 4
+            content.Add("endobj");
+
+            // Add Page object
+            content.Add("3 0 obj <</Type /Page /Parent 2 0 R /Resources 4 0 R /MediaBox [0 0 500 800] /Contents 6 0 R>>");
+            content.Add("endobj");
+
+            // Add Font Resource object
+            content.Add("4 0 obj <</Font <</F1 5 0 R>>>>");
+            content.Add("endobj");
+
+            // Add Font object
+            content.Add("5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Helvetica>>");
+            content.Add("endobj");
+
+        }
+
+        public async void Create()
+        {
+            var file = await Windows
+                   .Storage.ApplicationData.Current
+                   .LocalFolder.CreateFileAsync(location, CreationCollisionOption.ReplaceExisting);
+        }
+
+        public void Add(Paragraph p)
+        {
+            string temp = objectCounter.ToString() + " 0 obj";
+            content.Add(temp);
+
+            int tempLength = 0;
+            List<string> paragraph = p.GetContent();
+            foreach(string s in paragraph)
+            {
+                content.Add(s);
+                tempLength += s.Length;
+            }
+            xref.Add(tempLength);
+        }
+
+        public async void Close()
+        {
+            int xrefOffset = 0;
+            foreach(string s in content)
+            {
+                xrefOffset += Encoding.UTF8.GetByteCount(s);
+            }
+            
+            List<string> xrefTable = xref.Close(xrefOffset);
+            foreach(string s in xrefTable)
+            {
+                content.Add(s);
+            }
+
+            var file = await Windows
+                   .Storage.ApplicationData.Current
+                   .LocalFolder.CreateFileAsync(location, CreationCollisionOption.ReplaceExisting);
+
+            
+            var stream = await file.OpenAsync(FileAccessMode.ReadWrite);
+
+            using (var outputStream = stream.GetOutputStreamAt(0))
+            using (StreamWriter writer = new StreamWriter(outputStream.AsStreamForWrite()))
+            {
+                foreach (string s in content)
+                {
+                    writer.WriteLine(s);
+                }
+                writer.Dispose();
+            }
+
+            
+        }
+    }
+
+    public class Xref
+    {
+        int length = 0;
+        int count = 6;
+        List<string> table;
+
+        public Xref()
+        {
+            table = new List<string>();
+            table.Add("xref");
+            table.Add("0 " + count); // xref object count: This is line 2
+            table.Add("0000000000 65535 f");
+            table.Add("0000000009 00000 n");
+            table.Add("0000000056 00000 n");
+            table.Add("0000000111 00000 n");
+            table.Add("0000000212 00000 n");
+            table.Add("0000000250 00000 n");
+            length = 317;
+        }
+
+        public void Add(int objectLength)
+        {
+            // Change object count
+            count++;
+            string temp = "0 " + count.ToString();
+            table[1] = temp;
+
+
+            table.Add(length.ToString("D10") + " 00000 n");
+            length += objectLength;
+        }
+
+        public List<string> Close(int offset)
+        {
+            table.Add("trailer <</Size " + count + "/Root 1 0 R>>");
+            table.Add("startxref");
+            table.Add(offset.ToString());
+            table.Add("%%EOF");
+
+            return table;
+        }
+    }
+}
---
+++ Paragraph.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfWriter
+{
+    public class Paragraph
+    {
+        List<string> content;
+        int contentLength;
+
+        public Paragraph()
+        {
+            content = new List<string>();
+            content.Add("<</Length " + contentLength + ">>");
+            content.Add("stream");
+
+        }
+
+        public void Add(string s)
+        {
+            List<string> extra;
+            if(s.Length > 72)
+            {
+                extra = splitByLength(s, 72);
+                string temp = "BT /F1 12 Tf 50 750 Td 12 TL (" + extra[0] + ") Tj";
+                content.Add(temp);
+                contentLength += Encoding.UTF8.GetByteCount(temp);
+                for(int i = 1; i < extra.Count; i++)
+                {
+                    temp = "(" + extra[i] + ") '";
+                    content.Add(temp);
+                    contentLength += Encoding.UTF8.GetByteCount(temp);
+                }
+                content.Add("ET");
+                contentLength += Encoding.UTF8.GetByteCount("ET");
+                content[0] = "<</Length " + contentLength + ">>";
+            }
+            else
+            {
+                string temp = "BT /F1 12 Tf 50 750 (" + s + ") Tj ET";
+                content.Add(temp);
+                contentLength += Encoding.UTF8.GetByteCount(temp);
+                content[0] = "<</Length " + contentLength + ">>";
+            }
+        }
+
+        private List<string> splitByLength(string s, int length)
+        {
+            List<string> result = new List<string>();
+            if(s.Length < length)
+            {
+                result.Add(s);
+                return result;
+            }
+            else
+            {
+                while(s.Length >= length)
+                {
+                    result.Add(s.Substring(0, length));
+                    s = s.Substring(length);
+                }
+                result.Add(s);
+                return result;
+            }
+        }
+
+        public void Close()
+        {
+            content.Add("endstream");
+            content.Add("endobj");
+        }
+
+        public List<string> GetContent()
+        {
+            return content;
+        }
+
+        public int GetContentLength()
+        {
+            return contentLength;
+        }
+
+    }
+}
